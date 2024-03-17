e6347ccbb05cf533397e4e14920d952e3d0c31f3
Ayath2555 <ayathasbika17@gmail.com>
2024-03-17 21:06:53 +0530

    Initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["*.{html,js}"],
+  theme: {
+    fontFamily:{
+      'custom':['young serif'],
+      'custom1':['outfit'],
+    }
+    // extend: {},
+  },
+  plugins: [],
+}
+
